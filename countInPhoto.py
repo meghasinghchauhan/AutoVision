aa46ff87c2761d2f636fad02dfe30eabe7d6d85c
MEGHA SINGH CHAUHAN <128323553+meghasinghchauhan@users.noreply.github.com>
2025-04-16 22:51:13 +0530

    Update countInPhoto.py

--- countInPhoto.py
+++ countInPhoto.py
@@ -5,6 +5,12 @@ from cvlib.object_detection import draw_bbox
 def count_cars_in_photo(image_path):
     """Detects and counts cars in an image."""
     im = cv2.imread(image_path)
-    bbox, label, conf = cv.detect_common_objects(im)
+    bbox, label, conf = cv.detect_common_objects(
+    im,
+    model='yolov3-tiny',
+    config='model/yolov3-tiny.cfg',
+    weights='model/yolov3-tiny.weights',
+    labels='model/coco.names'
+)
     output_image = draw_bbox(im, bbox, label, conf)
     return output_image, label.count('car')
